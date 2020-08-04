be2aea2401e3370c2803830a408a1eebad5a6ea9
User <User@leas-mbp-2.home>
2020-08-04 13:27:58 +0100

    add basic if

---
+++ basic.js
@@ -0,0 +1,13 @@
+var name = "John";
+var age = 6;
+
+if (age > 10)
+{
+    // if the condition is true, do this
+    console.log("Hello, " + name);
+}
+else
+{
+    // if the condition is false, do this
+    console.log("You're too young!");
+}
